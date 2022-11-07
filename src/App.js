7613df58db5b57061f012ed1890ef40aeecf0fed
Oğulcan Doğan <odogan@Ogulcan-MacBook-Air.local>
2022-11-07 12:36:09 +0300

    created header-footer components :tada:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import SearchMovie from "./components/SearchMovie";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [movieData, setMovieData] = useState([]);
+
+  const handleChange = (e) => {
+    //aradığımız harfler arası yarım saniyeden fazlaysa temizlesin
+    const searchTimeOut = setTimeout(() => {
+      setSearch(e.target.value);
+    }, 500);
+
+    return () => {
+      clearTimeout(searchTimeOut);
+    };
+  };
   return (
     <div>
-      <SearchMovie />
+      <Header handleChange={handleChange} />
+      <SearchMovie
+        search={search}
+        movieData={movieData}
+        setMovieData={setMovieData}
+      />
+      <Footer />
     </div>
   );
 }
